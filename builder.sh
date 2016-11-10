3d8ec5346b49d54759182873ba3ba961aca4efce
yura <yurkinnnn@gmail.com>
2016-11-10 23:17:38 +0200

    Changes in builder.sh

--- builder.sh
+++ builder.sh
@@ -2,3 +2,14 @@
 
 
 echo `date +%Y-%m-%d.%H:%M:%S`': Start deploy' >> bash-log.txt
+
+# locale bug
+export LC_ALL=C
+
+echo `date +%Y-%m-%d.%H:%M:%S`': Install python requirements' >> bash-log.txt
+pip3 install -r requirements.txt
+
+echo `date +%Y-%m-%d.%H:%M:%S`': Finish deploy' >> bash-log.txt
+echo '================================================' >> bash-log.txt
+
+exit 0
